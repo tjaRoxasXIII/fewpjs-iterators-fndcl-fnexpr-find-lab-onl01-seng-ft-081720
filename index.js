465ce3a3defff22b744123c1fac16af6d2dde579
Taylor <tjaroguetech@gmail.com>
2020-11-25 10:13:16 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find( search => search.result === "W" )
+  if (win) {
+    return win.year
+  }
+  else {
+    return undefined
+  }
+}
